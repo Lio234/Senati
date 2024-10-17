a408e88228299f52cf33a1f3a83712f554fb0e5e
Lio234 <113069894+Lio234@users.noreply.github.com>
2024-10-17 14:54:33 -0500

    Update ctr_borrar_prod.php

--- Pagina_PHP-main/paginas/controller/ctr_borrar_prod.php
+++ Pagina_PHP-main/paginas/controller/ctr_borrar_prod.php
@@ -3,7 +3,6 @@ include "../includes/cargar_clases.php";
 
 $crudproducto = new CRUDProducto();
 
-// Cambia "codprod" a "cod_prod" para que coincida con el nombre del parámetro pasado por la URL
 if (isset($_GET["cod_prod"])) {
     $cod_prod = $_GET["cod_prod"];
     $crudproducto->BorrarProducto($cod_prod);
