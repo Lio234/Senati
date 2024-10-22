cfb79b76cb8468cada71ad42c9da517aa7b81de3
victor <vcanchari38@gmail.com>
2024-10-22 11:14:25 -0500

    cambio de menu.php

--- Pagina_PHP-main/paginas/includes/menu.php
+++ Pagina_PHP-main/paginas/includes/menu.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
+<nav class="navbar navbar-expand-lg custom-navbar no-bootstrap-styles"> 
     <div class="container-fluid">
         <a class="navbar-brand" href="https://www.senati.edu.pe/" target="_blank">
-            SENATI
+            COMPUWARE S.A.C.
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"><i class="fas fa-bars fa-lg"></i></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -12,22 +12,22 @@
                     <a class="nav-link" href="<?=$ruta?>/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?=$ruta?>/producto"> Productos </a>
+                    <a class="nav-link" href="<?=$ruta?>/producto">Productos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?=$ruta?>/marca"> Marcas </a>
+                    <a class="nav-link" href="<?=$ruta?>/marca">Marcas</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?=$ruta?>/categoria"> Categoria </a>
+                    <a class="nav-link" href="<?=$ruta?>/categoria">Categoria</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?=$ruta?>/cliente"> Clientes </a>
+                    <a class="nav-link" href="<?=$ruta?>/cliente">Clientes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?=$ruta?>/empleado"> Empleados </a>
+                    <a class="nav-link" href="<?=$ruta?>/empleado">Empleados</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?=$ruta?>/Pedido"> Pedidos </a>
+                    <a class="nav-link" href="<?=$ruta?>/pedido">Pedidos</a>
                 </li>                
             </ul>
         </div>
