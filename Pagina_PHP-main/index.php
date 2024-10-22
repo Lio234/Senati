eb77c87240cc2f51cac18d9ab03530d43acb055e
victor <vcanchari38@gmail.com>
2024-10-22 10:59:55 -0500

    landy page index.php

--- Pagina_PHP-main/index.php
+++ Pagina_PHP-main/index.php
@@ -9,54 +9,85 @@
         <?php 
         include "paginas/includes/menu.php";
         ?>
-        <div class="container mt-3">
-            <header>
-                <h1><i class="fas fa-university"></i><?=$titulo?></h1>
-                <hr/>
-            </header>
-
-            <section>
-                <article>
-                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" style="max-width: 75%; margin: 0 auto;">
-                        <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="img/imagen1.jpg" class="d-block w-100" style="height: 450px; object-fit: cover;" alt="Imagen 1">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Primera Diapositiva</h5>
-                                    <p>Texto descriptivo para la primera imagen.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img/imagen2.jpg" class="d-block w-100" style="height: 450px; object-fit: cover;" alt="Imagen 2">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Segunda Diapositiva</h5>
-                                    <p>Texto descriptivo para la segunda imagen.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img/imagen3.jpg" class="d-block w-100" style="height: 450px; object-fit: cover;" alt="Imagen 3">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Tercera Diapositiva</h5>
-                                    <p>Texto descriptivo para la tercera imagen.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Anterior</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Siguiente</span>
-                        </button>
-                    </div>
-                </article>
-            </section>
+    <section id="services" class="container">
+        <h2 class="text-center">Servicios</h2>
+        <div class="service-container">
+            <div class="service-item">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjrXZkXi5r5fcqu9jstnNRpOM0bub-klDJSw&s" alt="Compra y venta de hardware">
+                <h4>Compra y venta de hardware</h4>
+            </div>
+            <div class="service-item">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK_b8qOBjZw2FQBuI2ccl8wKXeCXVg7wWRsQ&s" alt="Mantenimiento y actualización">
+                <h4>Mantenimiento y actualización</h4>
+            </div>
+            <div class="service-item">
+                <img src="https://www.intel.la/content/dam/www/central-libraries/us/en/images/2023-01/s9-u05-29-install-gpu-original-rwd.jpg.rendition.intel.web.480.270.jpg" alt="Ensamblaje de PC's">
+                <h4>Ensamblaje de PC's</h4>
+            </div>
+            <div class="service-item">
+                <img src="https://zonagamer.tiendada.com/api/scrooge/file/FL-427AD6BD" alt="Cotizaciones personalizadas">
+                <h4>Cotizaciones personalizadas</h4>
+            </div>
         </div>
+    </section>
+
+    <section id="locations" class="container">
+    <h2 class="text-center">Nuestras sedes</h2>
+    <div class="location-container">
+        <div class="location-item">
+            <img class="location-img" src="https://larepublica.cronosmedia.glr.pe/original/2022/08/18/62ffa860404dee685359a238.jpg" alt="Sede Wilson">
+            <h3 class="location-title">Wilson</h3>
+            <p class="location-description">Ubicada en una zona céntrica, abarca 50 metros cuadrados y se dedica a la gestión eficiente de operaciones y parte de nuestro stock.</p>
+        </div>
+        <div class="location-item">
+            <img class="location-img" src="https://thumbs.dreamstime.com/b/tiendas-de-piezas-pc-en-la-plaza-digital-guanghua-taipei-taiwan-diciembre-gente-visita-las-mercado-computadoras-es-el-coraz%C3%B3n-del-271957500.jpg" alt="Sede Callao">
+            <h3 class="location-title">Callao</h3>
+            <p class="location-description">Destinada a la importación y exportación de productos, esta sede gestiona el almacenamiento del resto del inventario y realiza actividades similares.</p>
+        </div>
+    </div>
+</section>
+
+<section id="team" class="container">
+    <h2 class="text-center">Nuestro equipo</h2>
+    <p class="text-center">Contamos con un equipo de 20 trabajadores altamente capacitados:</p>
+    <ul>
+        <li>20% en atención al cliente</li>
+        <li>50% en mantenimiento de equipos</li>
+        <li>30% en transporte de equipos</li>
+    </ul>
+</section>
+
+<section id="history" class="container">
+    <h2 class="text-center">Nuestra historia</h2>
+    <img class="history-img" src="https://www.lostiempos.com/sites/default/files/styles/noticia_detalle/public/media_imagen/2020/5/22/11_p_1_lopezzzz.jpg?itok=CgGtX4qz" alt="Nuestra historia">
+    <p>Fundada en Lima en noviembre de 2019, COMPUWARE S.A.C. ha trabajado incansablemente para posicionarse como la opción preferida entre los clientes. Nos destacamos por ofrecer una amplia gama de componentes de hardware y servicios innovadores.</p>
+</section>
+
+<section id="mission-vision" class="container">
+    <h2 class="text-center">Misión</h2>
+    <img class="mission-img" src="https://tvazteca.brightspotcdn.com/dims4/default/023b34e/2147483647/strip/true/crop/1600x1200+0+0/resize/928x696!/format/jpg/quality/90/?url=http%3A%2F%2Ftv-azteca-brightspot.s3.amazonaws.com%2F69%2F5c%2F36ea59b54191a3706219eb6ab1f1%2Fwhatsapp-image-2022-12-27-at-18.39.14.jpeg" alt="Nuestra misión">
+    <p>Convertirnos en la primera opción para quienes buscan hardware, manteniendo siempre la honestidad y el respeto por el presupuesto de nuestros clientes.</p>
+    <h2 class="text-center">Visión</h2>
+    <img class="vision-img" src="https://i0.wp.com/www.parapcgamers.com/wp-content/uploads/2017/01/partes-de-hardware-en-negocio-de-computacion.jpg?ssl=1" alt="Nuestra visión">
+    <p>Posicionar nuestra empresa a nivel global, asegurando que nuestra marca sea sinónimo de los más altos estándares de seguridad y confianza en el mercado.</p>
+</section>
+
+<section id="values" class="container">
+    <h2 class="text-center">Valores</h2>
+    <ul>
+        <li>Innovación</li>
+        <li>Calidad</li>
+        <li>Satisfacción del cliente</li>
+        <li>Honestidad</li>
+        <li>Compromiso</li>
+        <li>Confianza</li>
+        <li>Seguridad</li>
+    </ul>
+</section>
+
+
+    <footer>
+        <p>&copy; 2024 COMPUWARE S.A.C. Todos los derechos reservados.</p>
+    </footer>
     </body>
 </html>
