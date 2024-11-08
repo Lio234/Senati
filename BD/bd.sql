4875853cfb64b297c813e1b76eeff7fdb2a0b053
victor <vcanchari38@gmail.com>
2024-11-08 10:50:48 -0500

    tabla usuario

--- BD/bd.sql
+++ BD/bd.sql
@@ -95,3 +95,13 @@ CREATE TABLE
         FOREIGN KEY (id_producto) REFERENCES tb_producto (id_producto),
         FOREIGN KEY (id_pedido) REFERENCES tb_pedido (id_pedido)
     );
+    
+--Tabla de Usuarios
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+); 
